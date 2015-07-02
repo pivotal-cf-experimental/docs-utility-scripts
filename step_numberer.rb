9f2819fd151e017734ef195e3d8c9012eeb5bc81
Michael Trestman <michael.a.trestman@gmail.com>
2015-07-02 10:23:35 -0700

    creates step_numberer

---
+++ step_numberer.rb
@@ -0,0 +1,35 @@
+# this utility takes a filename and renumbers all of the steps that are formatted the way we format them in docs, i.e. a link immediately followed by the word Step with a number. it beats doing it by hand.
+
+target_file = ARGV[0]
+text = File.read(target_file)
+puts text
+
+step_regex = /## <a id='.*?a>\s*?Step \d+/
+
+steps = text.scan step_regex
+
+puts "steps before:"
+
+steps_before = steps.clone.map { |step| step.clone }
+p steps_before
+steps_before.each { |step| p step }
+
+steps_after = steps.clone.each_with_index.map do |step,i|
+	step.sub!(/Step \d+/, "Step #{(i+1).to_s}")
+end
+
+puts "steps after:"
+p steps_after
+steps_after.each { |step| p step }
+
+steps_before.each_with_index do |sb,i|
+	puts 'searching text for:'
+	puts sb
+	match = text.scan sb
+	raise 'nothing found' if match.length == 0
+	text.sub! sb, steps_after[i]
+end
+
+puts text
+
+File.open(target_file, "w") { |file| file << text }
