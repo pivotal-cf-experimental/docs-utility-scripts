8393442d9a8daf918602863c0712514cb53c8a5c
Michael Trestman <michael.a.trestman@gmail.com>
2015-10-28 15:49:07 -0700

    adds single page app proof of concept

--- repo_checker.rb
+++ repo_checker.rb
@@ -16,7 +16,11 @@ class RepoChecker
     puts "the following repos are even_steven:"
     sorted_repos[:even_steven].each { |esr| puts "\t#{esr.path}" }
 
-    @repos.each do |repo|
+    puts "the following repos need a pull:"
+    sorted_repos[:needs_pull].each { |npr| puts "\t#{npr.path}"}
+
+
+    (@repos - sorted_repos[:needs_pull] - sorted_repos[:even_steven] ) .each do |repo|
       # sort_repos
       puts "||||||||||||||||||||||||||"
       puts "Hi I'm a repo! My path is #{repo.path}!\n Here's my status:\n"
@@ -44,7 +48,10 @@ class RepoChecker
     
     repos.each do |repo|
       even_steven << repo if repo.status == "On branch master\nYour branch is up-to-date with 'origin/master'.\nnothing to commit, working directory clean\n"
+      needs_pull << repo if repo.status.include? "On branch master\nYour branch is behind 'origin/master'"
     end
+
+
 
     {
       needs_push: needs_push,
@@ -120,6 +127,24 @@ class Repo
 
 end
 
+def interact rc
+
+  puts "what would you like to do next?"
+
+  puts "1: exit RepoChecker"
+  puts "2: view statuses of repos that need a pull"
+  puts "3: stash local changes, rebase, then stash pop all repos"
+  print "your response:"
+
+  response = gets.chomp
+  
+  if response == "3"
+    rc.stash_rebase_stashpop 
+    rc.report
+    interact rc
+  end
+
+end
 
 
 rc = RepoChecker.new
@@ -130,64 +155,7 @@ rc.fetch_statuses_concurrently
 
 rc.report
 
+interact rc
 
 
 
-# def find_workspace
-#   home = `pwd`
-#   dirs = home.split('/')
-#   top_dir = dirs.last.gsub(/\n/, '')
-
-#   return :top if dirs.length <= 1
-#   return :workspace if top_dir == "workspace"
-
-#   if home.include? "workspace"
-#     Dir.chdir '../'
-#     find_workspace
-#   end
-# end
-
-# raise "cannot find workspace directory" unless find_workspace == :workspace
-
-# results = Dir.glob "docs-*"
-
-# home = `pwd`.gsub("\n",'')
-# result_objects = results.map{ |result|
-#   results = {}
-#   Dir.chdir result
-#   results[:pwd] = Dir.pwd.gsub("\n",'')
-#   `git fetch`
-#   results[:git_status] = `git status`
-#   Dir.chdir home
-#   results
-# }
-
-# not_ahead = "nothing to commit"
-# not_behind = "Your branch is up-to-date with 'origin/master'."
-
-# result_objects.reject!{ |result|
-#   result[:git_status].include?(not_ahead) && result[:git_status].include?(not_behind)
-# }
-
-# puts "all repos are up-to-date with origin/master and have no changes to commit" if result_objects.length == 0
-
-# result_objects.each{|result|
-#   puts "***************************"
-#   result.keys.each {|key| puts "#{key} : #{result[key]}\n\n" }
-
-#   find_workspace
-#   Dir.chdir result[:pwd]
-
-#   `open -a gitx .` if ARGV.any?{|v| v=='-gitx' }
-
-#   # if ARGV.any?{|v| v=='-add-and-commit' }
-#   #   puts "adding all and committing that shiz"
-#   #   `git add .; git commit -m " removes beta indication from mentions of Diego [#100610688]"; git push`
-#   # end
-
-#   # if ARGV.any?{|v| v=='-push' }
-#   #   `git push`
-#   # end
-
-#   puts "$$$$$$$$$$$$$$$$$$$$$$$$"
-# } 
