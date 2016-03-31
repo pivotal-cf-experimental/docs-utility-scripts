57c59ad2273543089008ce68bdf50e115157d961
jbheron <jessbheron@gmail.com>
2016-03-31 16:27:21 -0700

    fixes stuff

--- step_numberer.rb
+++ step_numberer.rb
@@ -4,7 +4,7 @@ target_file = ARGV[0]
 text = File.read(target_file)
 puts text
 
-step_regex = /## <a id='.*?a>\s*?Step \d+/
+step_regex = /## <a id=[",'].*?a>\s*?Step \d+/
 
 steps = text.scan step_regex
 
@@ -21,15 +21,16 @@ end
 puts "steps after:"
 p steps_after
 steps_after.each { |step| p step }
-
+counter = 0
 steps_before.each_with_index do |sb,i|
 	puts 'searching text for:'
 	puts sb
 	match = text.scan sb
 	raise 'nothing found' if match.length == 0
 	text.sub! sb, steps_after[i]
+	counter += 1
 end
 
 puts text
-
+puts "Counter is #{counter}"
 File.open(target_file, "w") { |file| file << text }
