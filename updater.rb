7c3dc3d38761133bda08d69e56df2d73208b67ed
jbheron <jessbheron@gmail.com>
2016-06-28 21:38:59 -0700

    Adds multi-threading to updater.rb.

--- updater.rb
+++ updater.rb
@@ -3,13 +3,14 @@
 
 require 'pp'
 require 'yaml'
+# require 'open3'
 
 # Add new books to this array, as necessary
 @books = ['docs-book-cloudfoundry', 'docs-book-pcfservices', 'docs-book-pivotalcf', 'docs-book-runpivotal']
 @modified_repos = []
 @repo_list = ["docs-layout-repo", "docs-utility-scripts"]
 
-# Create a list of the book repositories to be cloned_or_updated, and send them to cloner/updater.
+# Create a list of the book repositories to be cloned_or_updated, send them to cloner/updater, and display the ignored modified repos.
 def gather_repos(books)
 	books.map do |book|
 		YAML.load(File.open(Dir.home + '/workspace/' + book + '/config.yml'))['sections'].each do |section| 
@@ -17,23 +18,30 @@ def gather_repos(books)
 		end
 	end
 	reduced_list = reduce_list_for_current_work @repo_list
-	clone_or_update reduced_list.uniq
+	multithread_pipe reduced_list.uniq
+	# clone_or_update reduced_list.uniq
 	display_modified_repos @modified_repos
 end
 
 # Removes repos with changes from @repo_list 
 def reduce_list_for_current_work(working_repos_array)
 	working_repos_array.reject! do |repo|
-		repo &&	@modified_repos.push(repo) if `cd ~/workspace/#{repo.gsub(/\w*-?\w*\//,'')}; git status`.include?('modified')			
+		repo &&	@modified_repos.push(repo) if File.directory?(Dir.home + '/workspace/' + repo.gsub(/\w*-?\w*\//,'')) && `cd ~/workspace/#{repo.gsub(/\w*-?\w*\//,'')}; git status`.include?('modified')		
 	end
 	working_repos_array.compact.uniq
 end
 
+def multithread_pipe(list_of_repos)
+	threads = []
+	list_of_repos.each do |repo|
+		threads << Thread.new { clone_or_update repo}
+	end
+	threads.each{|t|t.join}
+end
+
 # Ternary operation that checks for directory existence, if none, clones; otherwise updates repo
-def clone_or_update(repo_list)
-	repo_list.each do |repo|
+def clone_or_update(repo)
 		File.directory?(Dir.home + '/workspace/' + repo.gsub(/\w*-?\w*\//,'')) ? update_repo(repo) : clone_repo(repo) 
-	end	
 end
 
 # Displays repos with modifications
