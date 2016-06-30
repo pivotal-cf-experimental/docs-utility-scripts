a7300e95c1f1dd1a68cedb9b62d9073974a57bba
jbheron <jessbheron@gmail.com>
2016-06-29 19:29:19 -0700

    Adds self-update as first action for Updater Suite®.

--- updater.rb
+++ updater.rb
@@ -1,3 +1,9 @@
+# Copy/paste the following line to your shell (without the #) to add `update` as an alias:
+# echo 'alias update="ruby ~/workspace/docs-utility-scripts/updater.rb"' >> ~/.bash_profile; source ~/.bash_profile
+
+# This file now acts as the updater for updater and the update functionality
+#   for docs repos lives in updater_core.rb, which this script calls.
+
 # Updates docs-utility-scripts
 def get_current_updater
 	puts "Getting newest version of Updater Suite® and docs-utility-scripts"
