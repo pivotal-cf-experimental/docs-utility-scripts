75a5ccd7b2373a96da5d244905159577c132cdcb
jbheron <jessbheron@gmail.com>
2017-01-04 17:39:19 -0800

    Updates rr with functional prime command

--- rollback_release/install/rollback_release.sh
+++ rollback_release/install/rollback_release.sh
@@ -6,4 +6,5 @@ MAGENTA='\033[0;35m'
 WHITE='\033[1;37m'
 NC='\033[0m' 
 echo 'alias rr="$HOME/workspace/docs-utility-scripts/rollback_release/rr"' >> ~/.bash_profile
+echo 'source ~/.bash_profile'
 printf "\n${MAGENTA}  The rollback_release app is ready to go!\n\n  You can now run it with 'rr' from any directory at the command line.${NC}\n"
--- rollback_release/lib/prime.sh
+++ rollback_release/lib/prime.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#   receives PRIMEDAPP, SPACE, & ORG for cf push and start app with test route 
+#   receives RR_DIR, PRIMEDAPP, SPACE, & ORG for cf push and start app with test route 
 MAGENTA='\033[0;35m'
 WHITE='\033[1;37m'
 NC='\033[0m' 
-set -e
-
-# Need to export path as necessary
-ANYPATH=''
+set -ex
 
 # push the app
-cf push $PRIMEDAPP -i 1 -m 256M -k 1024M -d cfapps.io -n $PRIMEDAPP-primed-for-prod
-# Show link for app
+cd $RR_DIR
+
+cf push $PRIMEDAPP -i 1 -m 256M -k 1024M -d cfapps.io -n $PRIMEDAPP-primed-for-prod 
+
 echo "Your app is available at http://$PRIMEDAPP-primed-for-prod.cfapps.io$ANYPATH."
