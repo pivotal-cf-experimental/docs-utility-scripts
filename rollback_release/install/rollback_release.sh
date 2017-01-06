3ba999f3ceeea28baa074a1aba34ae90816ff1ce
jbheron <jessbheron@gmail.com>
2017-01-05 16:31:01 -0800

    Uncomments config, cleans up install script and some text.

--- rollback_release/install/rollback_release.sh
+++ rollback_release/install/rollback_release.sh
@@ -9,4 +9,4 @@ WHITE='\033[1;37m'
 NC='\033[0m' 
 echo 'alias rr="env RR_PWD=$PWD ruby $HOME/workspace/docs-utility-scripts/rollback_release/rr"' >> ~/.bash_profile
 echo 'source ~/.bash_profile'
-printf "\n${MAGENTA}  The rollback_release app is ready to go!\n\n  You can now run it with 'rr' from any directory at the command line.${NC}\n"
+printf "\n${MAGENTA}  The rollback_release app is ready to go!\n\n  You may need to run 'source ~/.bash_profile' to refresh your shell. \n\n You can run it with the 'rr' command from any directory at the command line. .${NC}\n"
