b33d2a6839532cd2349e4dd574df26ec2da52127
jbheron <jessbheron@gmail.com>
2017-01-16 16:10:30 -0800

    edge case for alpha docs config

--- rollback_release/lib/prime.sh
+++ rollback_release/lib/prime.sh
@@ -4,23 +4,24 @@ MAGENTA='\033[0;35m'
 WHITE='\033[1;37m'
 NC='\033[0m' 
 
-set -e
+set -ex
 
-# echo "inputs: 
-# 	SUBDOMAIN: $SUBDOMAIN
-#   RR_DIR: $RR_DIR
-#   PRIMEDAPP: $PRIMEDAPP
-#   ROUTE_PATH: $ROUTE_PATH
-#   DOMAIN: $DOMAIN
-#   SPACE: $SPACE
-#   ORG: $ORG"
+echo "inputs: 
+	SUBDOMAIN: $SUBDOMAIN
+  RR_DIR: $RR_DIR
+  PRIMEDAPP: $PRIMEDAPP
+  ROUTE_PATH: $ROUTE_PATH
+  DOMAIN: $DOMAIN
+  SPACE: $SPACE
+  ORG: $ORG"
 
 cd $RR_DIR
 
 # case to handle alpha-edge, the only product with subdomain and route_path in the .rr_config
 if [[ ${#SUBDOMAIN}>0 ]]
 	then
-	if [[ ${#ROUTE_PATH}>0 ]]
+
+	if [[ ${#ROUTE_PATH}>0 ]] || [[ $PRIMEDAPP == *"docs-edge-pcfservices"* ]]
 		then
 		cf push $PRIMEDAPP -i 1 -m 256M -k 1024M -d cfapps.io $SUBDOMAIN ${ROUTE_PATH/--path/--route-path}
 		echo "cf push $PRIMEDAPP -i 1 -m 256M -k 1024M -d cfapps.io -n $SUBDOMAIN ${ROUTE_PATH/--path/--route-path}"
