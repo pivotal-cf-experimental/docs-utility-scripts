0ab4d3ee771d85d832d117e5af665bf9e57e3101
Ben Tarnoff <btarnoff@pivotal.io>
2017-04-04 17:42:34 -0700

    update

--- stemcell-rn-bot/get-stemcells.rb
+++ stemcell-rn-bot/get-stemcells.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'octokit'
+require 'open-uri'
 require_relative 'update-stemcell-rn'
 
 @starting_stemcell_version = ENV['STARTING_STEMCELL_VERSION']
@@ -7,6 +8,28 @@ require_relative 'update-stemcell-rn'
 @path_to_stemcell_releases_json = 'docs-utility-scripts/stemcell-rn-bot/stemcell-releases.json'
 @existing_stemcell_json = JSON.load(File.open(@path_to_stemcell_releases_json)).to_json
 @client = Octokit::Client.new :access_token => ENV['STEMCELL_RN_BOT_GIT_TOKEN']
+
+def get_stemcells_pivnet
+
+  #get and parse the list of stemcell releases from pivnet
+  stemcells = URI.parse('https://network.pivotal.io/api/v2/products/stemcells/releases').read
+  stemcells = JSON.parse(stemcells) 
+  stemcells_list = stemcells['releases']
+
+  #put the list of stemcells in order of their version numbers
+  sorted_stemcells_list = stemcells_list.sort_by { |h| h['version'] }.reverse
+
+  right_stemcells = sorted_stemcells_list.select do |d|
+    d['version'].start_with?("#{@starting_stemcell_version}")
+  end
+
+  stemcells_numbers_list = right_stemcells.map do |r| 
+    r['version']
+  end
+
+  return stemcells_numbers_list
+
+end
 
 def get_stemcells
 
@@ -26,11 +49,22 @@ def get_stemcells
     d[:name] == "Stemcell #{@starting_stemcell_version}"
   end
 
+  # just the ones we want
   stemcell_releases_full = stemcell_releases_full[0..index_of_starting_stemcell]
+
+  # then a smaller slice of the stemcells on pivnet, plus the 'base' stemcell
+  stemcell_releases_slice = []
+  get_stemcells_pivnet.each do |n|
+    stemcell_releases_full.each do |s|
+      stemcell_releases_slice.push(s) if s[:name] == "Stemcell #{n}"
+    end
+  end
+
+  stemcell_releases_slice.push(stemcell_releases_full[index_of_starting_stemcell])
 
   # filter only name, date, and body info into new hash
   stemcell_releases = []
-  stemcell_releases_full.each do |d|
+  stemcell_releases_slice.each do |d|
     new_hash = d.select do |k,v| 
      [:name, :published_at, :body].include?(k)
     end
@@ -41,10 +75,25 @@ def get_stemcells
 
 end
 
+def update_stemcells_json(content)
+
+  utility_repo = Octokit::Repository.from_url('https://github.com/pivotal-cf-experimental/docs-utility-scripts')
+  stemcell_releases_file = @client.contents(utility_repo, {:path => 'stemcell-rn-bot/stemcell-releases.json'})
+
+  # update the stemcell release notes
+  @client.update_contents(utility_repo,
+                 "stemcell-rn-bot/stemcell-releases.json",
+                 "Stemcell RN Bot automatically updating stemcell-releases.json",
+                 stemcell_releases_file['sha'],
+                 content)
+end
+
 # if diffs between new stemcell info and existing stemcell info, then
 # write the new stemcell json and run the script that updates the stemcell releases notes
+
 new_stemcell_json = get_stemcells
 if new_stemcell_json != @existing_stemcell_json 
+  update_stemcells_json(new_stemcell_json)
   File.open(@path_to_stemcell_releases_json, 'w') {|f| f.write new_stemcell_json }
   @existing_stemcell_json = JSON.load(File.open(@path_to_stemcell_releases_json)).to_json
   build_new_rn
