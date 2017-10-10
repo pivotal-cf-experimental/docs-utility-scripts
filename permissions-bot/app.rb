82137354183e08a0eedaaa762180b67dd13bde6a
Ben Tarnoff <btarnoff@pivotal.io>
2017-10-10 11:48:08 -0700

    update permissions bot

--- permissions-bot/app.rb
+++ permissions-bot/app.rb
@@ -1,21 +1,17 @@
-require 'sinatra'
-require 'json'
 require 'octokit'
-require 'pp'
+require 'net/http'
+
+client = Octokit::Client.new :access_token => ENV["git_token"]
 
 
-post '/payload' do
+old_file = File.read('required_vcenter_privileges.md').gsub(/\s+/, "")
 
-  client = Octokit::Client.new :access_token => ENV["git_token"]
-  @push = JSON.parse(request.body.read) # push data
-  if @push["ref"] == "refs/heads/master"
-     @push["commits"].each do |f|
-     if f["modified"].include?("docs/required_vcenter_privileges.md")
-      client.create_issue("pivotal-cf/docs-pcf-install", 
+new_file_url = URI.parse('https://raw.githubusercontent.com/cloudfoundry-incubator/bosh-vsphere-cpi-release/master/docs/required_vcenter_privileges.md')
+
+new_file = Net::HTTP.get(new_file_url).gsub(/\s+/, "")
+
+if old_file != new_file
+  client.create_issue("pivotal-cf/docs-pcf-install", 
           "Change to vSphere Permissions", 
-          "The vSphere privileges [topic](https://github.com/cloudfoundry-incubator/bosh-vsphere-cpi-release/blob/master/docs/required_vcenter_privileges.md) in the `bosh-vsphere-cpi-release` GitHub repo has been modified. Please update the relevant versions of the [Installing Pivotal Cloud Foundry on vSphere](https://docs.pivotal.io/pivotalcf/customizing/vsphere.html#permissions) topic in the PCF Core Docs.")
-    end
-    end
-  end
-
+          "The vSphere privileges [topic](https://github.com/cloudfoundry-incubator/bosh-vsphere-cpi-release/blob/master/docs/required_vcenter_privileges.md) in the `bosh-vsphere-cpi-release` GitHub repo has been modified. Please update the relevant versions of the [vSphere Service Account Requirements](https://docs.pivotal.io/pivotalcf/customizing/vsphere-service-account.html) topic in the PCF Core Docs.")
 end
