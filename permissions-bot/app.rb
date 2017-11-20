97954d4d1133eff007c6c860fb66de7f5542481e
Ben Tarnoff <btarnoff@pivotal.io>
2017-11-20 12:15:19 -0800

    update vsphere permissions bot

--- permissions-bot/app.rb
+++ permissions-bot/app.rb
@@ -14,7 +14,13 @@ if old_file != new_file
   client.create_issue("pivotal-cf/docs-pcf-install", 
           "Change to vSphere Permissions", 
           "The vSphere privileges [topic](https://github.com/cloudfoundry-incubator/bosh-vsphere-cpi-release/blob/master/docs/required_vcenter_privileges.md) in the `bosh-vsphere-cpi-release` GitHub repo has been modified. Please update the relevant versions of the [vSphere Service Account Requirements](https://docs.pivotal.io/pivotalcf/customizing/vsphere-service-account.html) topic in the PCF Core Docs.")
-  File.open('docs-utility-scripts-master/permissions-bot/required_vcenter_privileges.md', 'w') {
-    |file| file.write(new_file)
-  }
+
+  markdown_file = client.contents('pivotal-cf-experimental/docs-utility-scripts', {:path => 'permissions-bot/required_vcenter_privileges.md'})
+
+  client.update_contents('pivotal-cf-experimental/docs-utility-scripts',
+                 "permissions-bot/required_vcenter_privileges.md",
+                 "vSphere Permissions Bot Updating File",
+                 markdown_file['sha'],
+                 new_file)
+
 end
