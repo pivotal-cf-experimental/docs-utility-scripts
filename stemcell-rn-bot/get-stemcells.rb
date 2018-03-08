c87a7544ded824bd050b6af1444ed904e626386b
Landon Jarzynski <ljarzynski@pivotal.io>
2018-03-07 16:30:58 -0800

    updates stemcell release checker script

--- stemcell-rn-bot/get-stemcells.rb
+++ stemcell-rn-bot/get-stemcells.rb
@@ -8,6 +8,7 @@ stemcells = URI.parse('https://network.pivotal.io/api/v2/products/stemcells/rele
 stemcells = JSON.parse(stemcells) 
 stemcells_list = stemcells['releases']
 today = Time.now.strftime("%Y/%m/%d").gsub!('/','-')
+release = false
 
 #put the list of stemcells in order of their version numbers
 sorted_stemcells_list = stemcells_list.sort_by { |h| h['release_date'] }.reverse
@@ -18,8 +19,12 @@ end
 
 stemcells_numbers_list = right_stemcells.map do |r|  
   if r['release_date'] == today
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"@docteam A new stemcell released today."}' https://hooks.slack.com/services/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
-  else
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"No new stemcell releases today."}' https://hooks.slack.com/services/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
+  	release = true
   end
+end
+
+if release then
+	puts `curl -X POST -H 'Content-type: application/json' --data '{"text":"@docteam A new stemcell released today."}' https://hooks.slack.com/services/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`
+else 
+	puts `curl -X POST -H 'Content-type: application/json' --data '{"text":"No new stemcells today."}' https://hooks.slack.com/services/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`
 end
