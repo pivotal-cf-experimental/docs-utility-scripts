9772ba0b0d33ab92c18530633e095b5718f36c68
Severine Tymon <s.tymon@gmail.com>
2019-10-11 16:32:48 -0700

    udpates the path to partials [#168879600]

--- deprecation-notice/deprecation.sh
+++ deprecation-notice/deprecation.sh
@@ -1,8 +1,9 @@
 # add deprecation partial to all files in current directory
 
+cd $1
 for file in $(find . -type f -name "*.html.md.erb"); 
 do
-  awk '/---/{x++} x==2{sub(/---/,"&\n\n<%= partial '\''../pcf/core/deprecation-notice'\'' %>")}1' $file > ${file}.tmp
+  awk '/---/{x++} x==2{sub(/---/,"&\n\n<%= partial '\''/shared/deprecation-notice'\'' %>")}1' $file > ${file}.tmp
   mv ${file}.tmp $file
 done
  
