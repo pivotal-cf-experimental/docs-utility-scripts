418d1400e5b9011053f0b6438a900c0b845aee18
Severine Tymon <s.tymon@gmail.com>
2019-10-21 14:52:49 -0700

    limit bad matches

--- deprecation-notice/deprecation.sh
+++ deprecation-notice/deprecation.sh
@@ -3,7 +3,7 @@
 cd $1
 for file in $(find . -type f -name "*.html.md.erb"); 
 do
-  awk '/---/{x++} x==2{sub(/---/,"&\n\n<%= partial '\''/shared/deprecation-notice'\'' %>")}1' $file > ${file}.tmp
+  awk '/^---$/{x++} x==2{sub(/---/,"&\n<%= partial '\''/pcf/deprecation-notice'\'' %>")}1' $file > ${file}.tmp
   mv ${file}.tmp $file
 done
  
