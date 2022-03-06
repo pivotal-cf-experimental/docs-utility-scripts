6da0c17c185e10166a07e19b4e574fd8ed85707f
RichardJJG <rijohnson@vmware.com>
2022-03-06 11:55:05 +0000

    Add Pivotal landing page section

--- configure-for-docworks/finisher.sh
+++ configure-for-docworks/finisher.sh
@@ -2,6 +2,10 @@ finisher () {
 
   # This script runs commands to make the migrated pages go "live" after you verify that the new pages on VMware look good.
   
+  # Update Pivotal Landing Page Section
+  # Change relevant values in https://github.com/pivotal-cf/docs-book-pcfservices/edit/master/master_middleman/source/index.html.erb.
+  # You need to plug your own values in here (likely just the product name for the docs repo you're dealing with).
+  
   # Make New Redirects Live Section
   # Delete "redirects.md" and rename "new-redirects.md" as "redirects.md".
 
