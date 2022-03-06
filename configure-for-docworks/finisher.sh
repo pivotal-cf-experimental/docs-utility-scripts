eec7eb2faff067f49244a5eabe7a03988cce7c5f
RichardJJG <rijohnson@vmware.com>
2022-03-06 11:48:34 +0000

    Create finisher.sh

---
+++ configure-for-docworks/finisher.sh
@@ -0,0 +1,8 @@
+finisher () {
+
+  # This script runs commands to make the migrated pages go "live" after you verify that the new pages on VMware look good.
+  
+  # Make New Redirects Live Section
+  # Delete "redirects.md" and rename "new-redirects.md" as "redirects.md".
+
+}
