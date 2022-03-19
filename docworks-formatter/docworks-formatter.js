a14d6d02aa07cec3320cf00eed030750c0814d7d
RichardJJG <rijohnson@vmware.com>
2022-03-19 23:37:09 +0000

    Add formatting fixes

--- docworks-formatter/docworks-formatter.js
+++ docworks-formatter/docworks-formatter.js
@@ -32,14 +32,36 @@ function replace(editor)
 function doreplacement(text)
 {
 
-  // Last updated: March 15, 2022
+  // Fix code snippet formats
 
-  text = text.replace(/(<pre class=.terminal.>([^<]*)</pre>)/gm,'```console$1```');
-  text = text.replace(/(```console([^`\n]*)```)/gm,'```console\n$1\n```');
-  text = text.replace(/(```console\n([^`]*)\n```\n([^\n]))/gm,'```console\n$1\n```\n\n$2');
-  text = text.replace(/(```\s*\$)/gm,'```console\n$');
-  text = text.replace(/(Where:[^\n]*\n([^\n]*\*)\s`)/gm,'Where:\n\n$1 `');
-  text = text.replace(/(\<%=\spartial\s"([^>]*)"\s%\>)/gm,'<%= partial '$1' %>');
+  text = text.replace(/```bash\b/gm,'```console');
+  text = text.replace(/```sh\b/gm,'```console');
+  text = text.replace(/```shell\b/gm,'```console');
+  text = text.replace(/\<pre class=.terminal.\>([^\<]*)\<\/pre\>/gm,'```console$1```');
+  text = text.replace(/```console([^`\n]*)```/gm,'```console\n$1\n```');
+  text = text.replace(/```\s*\$/gm,'```console\n$');
+
+  // [Replace asterisk and plus-sign bulletpoints with dashes because of a DocWorks bug]
+
+  // Add missing newlines
+
+  text = text.replace(/^([^\-\+\*\n].*)\n([\-\+\*].*\n[\-\+\*].*)/gm,'$1\n\n$2');
+  // [Insert newline above code snippet if missing]
+  text = text.replace(/```\w+\n([^`]*)\n```\n([^\n])/gm,'```\w+\n$1\n```\n\n$2');
+
+  // Fix broken partial formatting
+
+  text = text.replace(/\<%=\spartial\s"([^"]*)"\s%\>/gm,'<%= partial \'$1\' %>');
+
+  // Remove newlines within bullets
+
+  text = text.replace(/^(\s*[\*\-\+].*)\n([^\n\*\-\+].*)/gm,'$1 $2');
+  text = text.replace(/^(\s*[\*\-\+].*)\n([^\n\*\-\+].*)/gm,'$1 $2');
+  text = text.replace(/^(\s*[\*\-\+].*)\n([^\n\*\-\+].*)/gm,'$1 $2');
+  text = text.replace(/^(\s*[\*\-\+].*)\n([^\n\*\-\+].*)/gm,'$1 $2');
+  text = text.replace(/^(\s*[\*\-\+].*)\n([^\n\*\-\+].*)/gm,'$1 $2');
+
+  // [Change <p class="note"> and similar]
 
   return text;
 
